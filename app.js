961162de5c4393f540a59c6d6dee261e5540770d
VysochanskiyS <visosensey@gmail.com>
2021-10-14 16:07:02 +0300

    modules

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+import {Person,Film,Message } from './index.js'
+import {start,info_about,eye_color,height,gender} from './index.js'
+import { get_planet,planet_show,spin_planets } from './planet.js';
+// 
+
 const block = document.getElementById('block');
 const info_people = document.getElementById('info');
 const films = document.getElementById('films');
@@ -9,13 +14,8 @@ const spinner_planets = document.getElementById('spinner_planets');
 const about = document.getElementById('about');
 const film_names = document.getElementById('film_names');
 
-
-//
+//variables
 let spin = false;
-let planet_show = false;
-let spin_planets = false;
-
-
 //addEventListener
 info_people.addEventListener('click',(e)=>{
     block.style.display = 'block'
@@ -40,7 +40,6 @@ const data = () => {
   }).catch((error)=>{console.log(error)})}
 
 
-
 const list_people = (data)=>{ 
     spinner.style.display = 'none'
     spin = true; 
@@ -57,8 +56,6 @@ const list_people = (data)=>{
         })  
     });
 }
-
-
 
 // returns list of films
 const getFilms = (index,data)=>{
@@ -84,64 +81,6 @@ const getFilms = (index,data)=>{
         }).catch((error)=>{console.log(error)})
         })
         
-}
-// return list of planets
-const get_planet = ()=>{
-    axios('https://swapi.dev/api/planets').then((response)=>{
-        block.style.display = 'none'
-        films.style.display = 'none'
-        film_title.style.display = 'none'
-        spinner_planets.style.display = 'none'
-        planet_show = true; 
-        spin_planets = true;
-        response.data.results.forEach((item)=>{
-        planets.innerHTML += `<div>${item.name}</div>`
-         console.log(item)
-     })
-    }).catch((error)=>{console.log(error)})
-}
-
-let info_about;
-let start ;
-let eye_color;
-let height;
-let gender;
-
-
-//Class
-class Person {
-    constructor(color,height,gender){
-        this.color = color;
-        this.height = height;
-        this.gender = gender
-    };
-    sayAbout(){
-      start = "Hello "
-    };
-}
-
-
-class Film extends Person{
-constructor(name,color,height,male){
-    super(color,height,male)
-    this.name = name
-}
-
-sayHi(){
-    super.sayAbout()
-    info_about = `<span class='info_about_person' >${this.name}</span>`;
-    eye_color = this.color
-    height = this.height;
-    gender = this.gender;
-}
-}
-
-// closure
-const Message = (height)=>{
-  const message = ( height <150 ? " are small ": " are tall")
-    return function(name){
-        return name+message
-    }
 }
 
 
---
+++ index.js
@@ -0,0 +1,38 @@
+//Class
+
+export let start,info_about,eye_color,height,gender ;
+class Person {
+    constructor(color,height,gender){
+        this.color = color;
+        this.height = height;
+        this.gender = gender
+    };
+    sayAbout(){
+      start = "Hello "
+    };
+}
+
+  class Film extends Person{
+constructor(name,color,height,male){
+    super(color,height,male)
+    this.name = name
+                    }
+    sayHi(){
+    super.sayAbout()
+    info_about = `<span class='info_about_person' >${this.name}</span>`;
+    eye_color = this.color
+    height = this.height;
+    gender = this.gender;
+}
+}
+
+// closure
+ const Message = (height)=>{
+  const message = ( height <150 ? " are small ": " are tall")
+    return function(name){
+        return name+message
+    }
+}
+
+export {Person ,Film,Message }
+
